f619200a49d2fbd5e6ea95201b4ef7201b93c877
Łukasz Niezgoda <235584@student.pwr.edu.pl>
2021-01-29 17:14:19 +0100

    Init commit

---
+++ MassVaccination/Algorithms/BruteForce.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MassVaccination.Algorithms
+{
+    class BruteForce
+    {
+        // N
+        private double Population;
+
+        // S
+        private double Susceptible;
+
+        // I
+        private double Infected = 1;
+
+        // R
+        private double Recovered = 0;
+
+        // V
+        private double Vaccinated = 0;
+
+        public BruteForce()
+        {
+            this.Population = 1000;
+            this.Susceptible = this.Population - this.Infected - this.Recovered;
+        }
+
+        public BruteForce(double population, double infected, double vaccinated)
+        {
+            this.Population = population;
+            this.Infected = infected;
+            this.Vaccinated = vaccinated;
+            this.Susceptible = this.Population - this.Infected - this.Recovered;
+        }
+
+        public (double[] S, double[] I, double[] R) SimpleSIR(double beta, double gamma, int days)
+        {
+            // dS / dt = - (beta * S * I) / N
+            // dI / dt = (beta * S * I) / N - (gamma * I)
+            // dR / dt = gamma * I
+
+            int span = days + 1;
+
+            double[] S = new double[span];
+            double[] I = new double[span];
+            double[] R = new double[span];
+
+            S[0] = this.Susceptible;
+            I[0] = this.Infected;
+            R[0] = this.Recovered;
+
+            for (var i = 1; i < span; i++)
+            {
+                int dayBefore = i - 1;
+
+                S[i] = S[dayBefore] - beta * S[dayBefore] * I[dayBefore] / this.Population;
+                I[i] = I[dayBefore] + beta * S[dayBefore] * I[dayBefore] / this.Population - gamma * I[dayBefore];
+                R[i] = R[dayBefore] + gamma * I[dayBefore];
+
+                //Debug.WriteLine($"S: {S[i]}, I: {I[i]}, R: {R[i]}");
+            }
+
+
+            return (S, I, R);
+        }
+
+        public (double[] S, double[] V, double[] I, double[] R) SimpleSVIR(double beta, double omega, double gamma, int days, int startOfVaccineProgram)
+        {
+            // dS / dt = - (beta * S * I) / N - omega * S
+            // dV / dt = omega * S
+            // dI / dt = (beta * S * I) / N - (gamma * I)
+            // dR / dt = gamma * I
+
+            int span = days + 1;
+
+            double[] S = new double[span];
+            double[] V = new double[span];
+            double[] I = new double[span];
+            double[] R = new double[span];
+
+            S[0] = this.Susceptible;
+            V[0] = this.Vaccinated;
+            I[0] = this.Infected;
+            R[0] = this.Recovered;
+
+            for (var i = 1; i < span; i++)
+            {
+                int dayBefore = i - 1;
+                bool isStartOfVaccinationProgram = i > startOfVaccineProgram;
+
+                S[i] = S[dayBefore] - beta * S[dayBefore] * I[dayBefore] / this.Population - (isStartOfVaccinationProgram ? omega * S[dayBefore] : 0);
+                V[i] = V[dayBefore] + (isStartOfVaccinationProgram ? omega * S[dayBefore] : 0);
+                I[i] = I[dayBefore] + beta * S[dayBefore] * I[dayBefore] / this.Population - gamma * I[dayBefore];
+                R[i] = R[dayBefore] + gamma * I[dayBefore];
+
+                //Debug.WriteLine($"{i} - S: {S[i]}, V: {V[i]},  I: {I[i]}, R: {R[i]}");
+            }
+
+
+            return (S, V, I, R);
+        }
+    }
+}
+ 
---
+++ MassVaccination/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MassVaccination.Algorithms;
+using System.Text.RegularExpressions;
+
+namespace MassVaccination
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Parameters _params = new Parameters
+        {
+            Population = 1000,
+            InitialInfected = 1,
+            InitialVaccinated = 0,
+            Beta = 0.5,
+            Gamma = 0.01,
+            Omega = 0.1,
+            StartOfVaccinationProgram = 0,
+            Days = 365,
+            Models = new string[] { "Simple SIR", "Simple SVIR" }
+        };
+
+        public MainWindow()
+        {   
+            
+            InitializeComponent();
+            this.DataContext = this._params;
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("^[0-9]+([.,][0-9]+)?$");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        void Plot(object sender, RoutedEventArgs e)
+        {
+            switch (models.SelectedIndex)
+            {
+                case 0:
+                    PlotSIR();
+                    break;
+                case 1:
+                    PlotSVIR();
+                    break;
+                default:
+                    throw new ArgumentException("Unknown model");
+            }
+
+            plot.Render();
+        }
+
+        private void PlotSIR()
+        {
+            plot.plt.Clear();
+
+            var bf = new BruteForce(
+                this._params.Population,
+                this._params.InitialInfected,
+                this._params.InitialVaccinated
+            );
+
+            var (S, I, R) = bf.SimpleSIR(this._params.Beta, this._params.Gamma, this._params.Days);
+            var xs = DataGen.Consecutive(this._params.Days + 1);
+
+
+            plot.plt.PlotScatter(xs, S, label: "Susceptible");
+            plot.plt.PlotScatter(xs, I, label: "Infected");
+            plot.plt.PlotScatter(xs, R, label: "Recovered");
+            plot.plt.Legend();
+
+            plot.plt.Title("Simple SIR");
+            plot.plt.YLabel("Number of people");
+            plot.plt.XLabel("Days");
+        }
+
+        private void PlotSVIR()
+        {
+            plot.plt.Clear();
+
+            var bf = new BruteForce(
+                this._params.Population,
+                this._params.InitialInfected,
+                this._params.InitialVaccinated
+            );
+
+            var (S, V, I, R) = bf.SimpleSVIR(
+                this._params.Beta,
+                this._params.Omega,
+                this._params.Gamma, 
+                this._params.Days, 
+                this._params.StartOfVaccinationProgram
+            );
+            var xs = DataGen.Consecutive(this._params.Days + 1);
+
+
+            plot.plt.PlotScatter(xs, S, label: "Susceptible");
+            plot.plt.PlotScatter(xs, V, label: "Vaccinated");
+            plot.plt.PlotScatter(xs, I, label: "Infected");
+            plot.plt.PlotScatter(xs, R, label: "Recovered");
+            plot.plt.Legend();
+
+            plot.plt.Title("Simple SVIR");
+            plot.plt.YLabel("Number of people");
+            plot.plt.XLabel("Days");
+        }
+    }
+
+
+    class Parameters
+    {
+        public int Population { get; set; }
+        public double InitialInfected { get; set; }
+        public double InitialVaccinated { get; set; }
+        public double Beta { get; set; }
+        public double Gamma { get; set; }
+        public double Omega { get; set; }
+        public int Days{ get; set; }
+        public int StartOfVaccinationProgram { get; set; }
+        public string[] Models { get; set; }
+    }
+}
